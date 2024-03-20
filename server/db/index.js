abb77901f34ceb7dd05573fe7ac0a615a05addc5
Ryan Cokely <ryan.cokely@gmail.com>
2024-03-20 10:47:02 -0400

    db models

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,18 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Quarterback = require('./models/Quarterback')
+const Receiver = require('./models/Receiver')
 
 //associations could go here!
+Quarterback.hasMany(Receiver);
+Receiver.belongsTo(Quarterback);
 
 module.exports = {
   db,
   models: {
     User,
+    Quarterback,
+    Receiver
   },
 }
