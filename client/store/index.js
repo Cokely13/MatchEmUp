ed25698ebce07ae8ffef2174c6b67d0e0aeafe46
Ryan Cokely <ryan.cokely@gmail.com>
2024-03-20 13:57:46 -0400

    redux store

--- client/store/index.js
+++ client/store/index.js
@@ -3,6 +3,7 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import singleQuarterbackReducer from './singleQuarterbackStore'
 
 const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
