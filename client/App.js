3d0d31b1ea6c040b17e608eb028073ab0ccd93be
Ryan Cokely <ryan.cokely@gmail.com>
2024-04-29 19:01:18 -0400

    homepage

--- client/App.js
+++ client/App.js
@@ -7,7 +7,7 @@ import Routes from './Routes'
 
 const App = () => {
   return (
-    <div >
+    <div  className="homepage">
       <div className="title-container">
         <h1 className="title">MatchEmUp</h1>
       </div>
