ac99b7d1ff5a69371517ce1d10e57d8385a4938f
Ryan Cokely <ryan.cokely@gmail.com>
2024-05-10 09:54:14 -0400

    added tv to selection

--- client/components/Home.js
+++ client/components/Home.js
@@ -97,20 +97,21 @@ export const Home = props => {
       'Music': '/music',
       'NBA': '/nba',
       'STATE': '/state',
+      'TV': '/show',
       'Random': getRandomPath()
     };
     history.push(paths[selection] || '/');
   };
 
   const getRandomPath = () => {
-    const paths = ['/cinema', '/qb', '/music', '/nba', '/state'];
+    const paths = ['/cinema', '/qb', '/music', '/nba', '/state', '/show'];
     const randomIndex = Math.floor(Math.random() * paths.length);
     return paths[randomIndex];
   };
 
-  const handleGuestEntry = () => {
-    history.push('/home'); // Redirect guests to the homepage
-  };
+  // const handleGuestEntry = () => {
+  //   history.push('/home'); // Redirect guests to the homepage
+  // };
 
   return (
     <div className="homepage">
@@ -124,11 +125,12 @@ export const Home = props => {
           <option value="Music">Music</option>
           <option value="NBA">NBA</option>
           <option value="STATE">STATE</option>
+          <option value="STATE">TV</option>
           <option value="Random">Random</option>
         </select>
         <button type="submit" className="match-button">Go</button>
       </form>
-      <button onClick={handleGuestEntry} className="guest-entry-button">Enter As Guest</button>
+      {/* <button onClick={handleGuestEntry} className="guest-entry-button">Enter As Guest</button> */}
     </div>
   );
 }
