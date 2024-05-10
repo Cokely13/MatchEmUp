291ec0995f29c7ee4a72ee1627c25d31d8dd040d
Ryan Cokely <ryan.cokely@gmail.com>
2024-05-10 10:51:04 -0400

    added navbar

--- client/App.js
+++ client/App.js
@@ -1,20 +1,45 @@
-import React from 'react'
+// import React from 'react'
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+// import Navbar from './components/Navbar'
+// import Routes from './Routes'
 
 
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div  className="homepage">
+//       <div className="title-container">
+//         <h1 className="title">MatchEmUp</h1>
+//       </div>
+//       <Navbar />
+//       <Routes />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { connect } from 'react-redux';
+import Navbar from './components/Navbar';
+import Routes from './Routes';
+
+const App = ({ isLoggedIn, isGuest }) => {
   return (
-    <div  className="homepage">
+    <div className="homepage">
       <div className="title-container">
         <h1 className="title">MatchEmUp</h1>
       </div>
-      <Navbar />
+      {(isLoggedIn || isGuest) && <Navbar />}
       <Routes />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  isLoggedIn: !!state.auth.id,
+  isGuest: state.auth.isGuest
+});
+
+export default connect(mapStateToProps)(App);
+
